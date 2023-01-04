c036feb57d7ebdc76626675d62df960a633699c8
Neil Christian Riego <neilchristianriego3@gmail.com>
2023-01-04 22:00:54 +0800

    test

---
+++ utils/trial-bs4.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+import os
+import sys
+
+URL = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+try:
+    html = requests.get(URL).text
+    document = bs4.BeautifulSoup(html, 'html.parser')
+    table = document.find('table', class_='infobox vevent')
+    python_url = table.find('th', text='Website').next_sibling.a['href']
+    version = table.find(
+        'th', text='Stable release').next_sibling.strings.__next__()
+    logo_url = table.find('img')['src']
+    logo = requests.get(f'https:{logo_url}').content
+    filename = os.path.basename(logo_url)
+    with open(filename, 'wb') as file:
+        file.write(logo)
+    print(f'{python_url}, {version}, file://{os.path.abspath(filename)}')
+except requests.exceptions.ConnectionError:
+    print("You've got problems with connection.", file=sys.stderr)
