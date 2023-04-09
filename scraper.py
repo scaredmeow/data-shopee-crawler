43ae2ec879a12c439f6f060ef03ac9cb9ddd7232
Neil Christian Riego <neilchristianriego3@gmail.com>
2023-04-10 01:01:47 +0800

    Update scraper to catch TypeError and some code in main and requirements

--- scraper.py
+++ scraper.py
@@ -56,37 +56,67 @@ class ShopeeAPI:
             ).json()
 
             i = 1
-            for i, rating in enumerate(data["data"]["ratings"], 1):
-                data_format["userid"].append(rating["userid"])
-                data_format["username"].append(rating["author_username"])
-                data_format["item"].append(rating["product_items"][0]["name"])
-                data_format["item_type"].append(idx)
-                data_format["ctime"].append(rating["ctime"])
-                data_format["comment"].append(rating["comment"])
-                data_format["rating"].append(rating["rating_star"])
-                data_format["product_quality"].append(
-                    rating["detailed_rating"]["product_quality"]
-                )
-                data_format["seller_service"].append(
-                    rating["detailed_rating"]["seller_service"]
-                )
-                data_format["delivery_service"].append(
-                    rating["detailed_rating"]["delivery_service"]
-                )
-                data_format["has_template_tag"].append(rating["has_template_tag"])
-                data_format["template_tags"].append(rating["template_tags"])
-                data_format["tags"].append(rating["tags"])
-                data_format["is_oversea"].append(rating["sip_info"]["is_oversea"])
-                data_format["origin_region"].append(rating["sip_info"]["origin_region"])
-                data_format["like_count"].append(rating["like_count"])
-                data_format["is_repeated_purchase"].append(
-                    rating["is_repeated_purchase"]
-                )
-                data_format["exclude_scoring_due_low_logistic"].append(
-                    rating["exclude_scoring_due_low_logistic"]
-                )
+            try:
+                for i, rating in enumerate(data["data"]["ratings"], 1):
+                    data_format["userid"].append(rating["userid"])
+                    data_format["username"].append(rating["author_username"])
+                    data_format["item"].append(rating["product_items"][0]["name"])
+                    data_format["item_type"].append(idx)
+                    data_format["ctime"].append(rating["ctime"])
+                    data_format["comment"].append(rating["comment"])
+                    data_format["rating"].append(rating["rating_star"])
+                    data_format["product_quality"].append(
+                        rating["detailed_rating"]["product_quality"]
+                    )
+                    data_format["seller_service"].append(
+                        rating["detailed_rating"]["seller_service"]
+                    )
+                    data_format["delivery_service"].append(
+                        rating["detailed_rating"]["delivery_service"]
+                    )
+                    data_format["has_template_tag"].append(rating["has_template_tag"])
+                    data_format["template_tags"].append(rating["template_tags"])
+                    data_format["tags"].append(rating["tags"])
+                    data_format["is_oversea"].append(rating["sip_info"]["is_oversea"])
+                    data_format["origin_region"].append(
+                        rating["sip_info"]["origin_region"]
+                    )
+                    data_format["like_count"].append(rating["like_count"])
+                    data_format["is_repeated_purchase"].append(
+                        rating["is_repeated_purchase"]
+                    )
+                    data_format["exclude_scoring_due_low_logistic"].append(
+                        rating["exclude_scoring_due_low_logistic"]
+                    )
 
-            if i % 20 or offset >= 3000:
+                if i % 20:
+                    df = pd.DataFrame(data_format)
+                    df.to_csv(self.dir + "/" + str(idx) + ".csv", index=False)
+                    data_format = {
+                        "userid": [],
+                        "username": [],
+                        "item": [],
+                        "item_type": [],
+                        "ctime": [],
+                        "comment": [],
+                        "rating": [],
+                        "product_quality": [],
+                        "seller_service": [],
+                        "delivery_service": [],
+                        "has_template_tag": [],
+                        "template_tags": [],
+                        "tags": [],
+                        "is_oversea": [],
+                        "origin_region": [],
+                        "like_count": [],
+                        "is_repeated_purchase": [],
+                        "exclude_scoring_due_low_logistic": [],
+                    }
+                    idx += 1
+                    offset = 0
+                else:
+                    offset += 20
+            except TypeError:
                 df = pd.DataFrame(data_format)
                 df.to_csv(self.dir + "/" + str(idx) + ".csv", index=False)
                 data_format = {
@@ -111,5 +141,3 @@ class ShopeeAPI:
                 }
                 idx += 1
                 offset = 0
-            else:
-                offset += 20
